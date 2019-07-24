72cd46d1bf96705cf23adadd02c00401b1cc2849
jiayizhg <jiayi.zhang@knights.ucf.edu>
2019-07-24 00:08:56 -0400

    modify home

--- src/app/task/home.component.ts
+++ src/app/task/home.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
+export interface Priority {
+    value: string;
+    viewValue: string;
+  }
+
 @Component({
+    selector: 'home.component',
     templateUrl: './home.component.html'
 })
 export class HomeComponent{
+    priorities: Priority[] = [
+        {value: 'steak-0', viewValue: 'Steak'},
+        {value: 'pizza-1', viewValue: 'Pizza'},
+        {value: 'tacos-2', viewValue: 'Tacos'}
+    ];
 }
